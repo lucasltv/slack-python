569c35aa48901af5e6bc05477e91a1ab0d33670a
Lucas Vieira <lucasltv@gmail.com>
2025-03-10 13:42:51 -0300

    Add Slack webhook integration and project documentation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Slack Webhook POC
+
+This project is a Proof of Concept (POC) for integrating Slack webhooks with a Python application.
+
+## Features
+
+- Send messages to a Slack channel using webhooks
+- Simple and easy-to-understand code
--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("OK")
+import os
+from slack_sdk.webhook import WebhookClient
+
+
+url = os.getenv(
+    "SLACK_WEBHOOK_URL",
+    "https://hooks.slack.com",
+)
+webhook = WebhookClient(url)
+response = webhook.send(
+    text="fallback",
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*",
+            },
+        }
+    ],
+)
